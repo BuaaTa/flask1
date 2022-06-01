f957ac1788e5742b8df6810126cc2e25db4e8c24
ta <630782761@qq.com>
2022-06-01 17:47:55 +0800

    第二次

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello Worldadddaadd!'
 
-这是第一个
-if __name__ == '__main__':
-    app.run()
+
+
